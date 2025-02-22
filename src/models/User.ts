e17182b846d74f78a4c753f7eb3a12c5b2d5b568
ViktoriiaIsk <vika.iskrich123@gmail.com>
2025-02-22 13:36:47 +0100

    Created User model

---
+++ src/models/User.ts
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+    {
+        firstName: { type: String, required: true },
+        lastName: { type: String, required: true },
+        email: { type: String, unique: true, required: true },
+        password: { type: String, required: true },
+        phone: { type: String },
+        role: { type: String, enum: ["customer", "admin"], default: "customer" },
+    },
+    { timestamps: true }
+);
+
+export const User = mongoose.model("User", UserSchema);
+
