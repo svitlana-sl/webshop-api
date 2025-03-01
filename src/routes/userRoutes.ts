1ec7868337777c76cd9b256991618a2f5020b2d0
ViktoriiaIsk <vika.iskrich123@gmail.com>
2025-03-01 15:15:04 +0100

    Added and tested GET /api/users and DELETE /api/users/:id routes

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { registerUser, loginUser } from "../controllers/userController";
+import { registerUser, loginUser, getUsers, deleteUser } from "../controllers/userController";
 import { RequestHandler } from "express"; // Import the RequestHandler type from Express
 const router = express.Router();
 
@@ -8,3 +8,9 @@ router.post("/register", registerUser as RequestHandler);
 // Route for user login
 router.post("/login", loginUser as RequestHandler);
 export default router;
+
+// Route to fetch all users (admin only in the future)
+router.get("/", getUsers as RequestHandler);
+
+// Route to delete a user by ID (admin only in the future)
+router.delete("/:id", deleteUser as RequestHandler);
